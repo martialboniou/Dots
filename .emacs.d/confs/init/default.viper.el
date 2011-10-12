5dcd8d6a4252ff4125f2de67d30665871ea53ae5
Martial <hondana@gmx.com>
2011-10-12 22:42:26 +0200

    viper fix in NO-WINDOW-SYSTEM > emacs

--- .emacs.d/confs/init/default.viper.el
+++ .emacs.d/confs/init/default.viper.el
@@ -1,12 +1,16 @@
-; Viper Mode setup
+;;; Viper Mode setup
 (setq-default viper-mode t
               viper-inhibit-startup-message 't
               viper-ex-style-editing nil
               viper-expert-level '5
               viper-syntax-preference 'extended
               viper-always t)
-
-; eshell is not a friend of Viper
+;; IMPORTANT: during NO-WINDOW-SYSTEM session:
+;;            1/ no `viper' in minibuffer; otherwise arrows = OD,OC.. => `ido' is broken
+;;            2/ use C-\ as <meta>-key
+(unless window-system
+  (setq viper-vi-style-in-minibuffer nil))
+;; eshell is not a friend of Viper
 (add-hook 'eshell-mode-hook
           (lambda ()
             (when viper-mode

9ecfa3b8169d984cf4cafaf8c5e77f6360c0e06b
martialboniou <hondana@gmx.com>
2013-12-08 15:18:27 +0100

    fix ruby install with chruby > dynamic > env > zsh

--- zsh/zsh.symlink/env/dynamic/00-ruby.zsh
+++ zsh/zsh.symlink/env/dynamic/00-ruby.zsh
@@ -1,6 +1,20 @@
+DEFAULT_RUBY_VERSION=2.0.0
+
 install_ruby_source () {
   [[ -s "$1" ]] && source "$1" && return 0
   return 1
+}
+
+_chruby_post_install () {
+  local version
+  local file
+  file="$HOME/.ruby-version"
+  if [[ -f "$file" ]]; then
+    version=$(cat "$file")
+  else
+    version=$DEFAULT_RUBY_VERSION
+  fi
+  [[ "$(chruby | grep $version 2>/dev/null)" == "" && "$version" != "$DEFAULT_RUBY_VERSION" ]] && chruby $DEFAULT_RUBY_VERSION || chruby $version
 }
 
 () {
@@ -9,10 +23,9 @@ install_ruby_source () {
   ruby_post_installs=( _chruby_post_install true )
   for (( i = 1; i <= ${#ruby_installs[@]}; i++ )); do
     if install_ruby_source "${ruby_installs[$i]}"; then
-      local post_install=${ruby_post_install[$i]}
-      chruby 2.0.0
-      # if [[ -f $post_install ]]; then
-      # return 0
+      local post_install=${ruby_post_installs[$i]}
+      eval "$post_install" 2>/dev/null
+      return
     fi
   done
 }

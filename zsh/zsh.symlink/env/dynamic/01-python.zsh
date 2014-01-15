421474775da00defeb5ad7fa259961be71bf1484
martialboniou <hondana@gmx.com>
2014-01-15 12:28:56 +0100

    fix 01-python > dynamic > env > zsh

--- zsh/zsh.symlink/env/dynamic/01-python.zsh
+++ zsh/zsh.symlink/env/dynamic/01-python.zsh
@@ -2,5 +2,5 @@
   local MAJOR=$(echo -e "import sys\nprint sys.version_info.major" | python 2>/dev/null)
   local MINOR=$(echo -e "import sys\nprint sys.version_info.minor" | python 2>/dev/null)
   local SITE="/usr/local/lib/python${MAJOR}.${MINOR}/site-packages"
-  [[ "$MAJOR" != "" ]] && [[ "$MINOR" != "" ]] && [[ -d "${SITE}" ]] && export PYTHONPATH="${SITE}:${PYTHONPATH}"
+  [[ "$MAJOR" != "" ]] && [[ "$MINOR" != "" ]] && [[ -d "${SITE}" ]] && PYTHONPATH="${SITE}" && typeset -U -g PYTHONPATH && export PYTHONPATH
 }
